8b4a2ee434c6a28750dc549e4962718bd8a3cd22
Antoniot <clementinapirlo3000@gmail.com>
2017-07-17 08:38:37 -0400

    Inicio del proyecto

---
+++ conexion/controlador/configuracion_bd_inicial.php
@@ -0,0 +1,18 @@
+<?php
+	$configuracion = (object) [
+		'servidor' => 'localhost',
+		'nombreBD' => 'proyectos',
+		'puerto' => '5432',
+		'nombreDeUsuario' => 'antoniot',
+		'contrasenia' => '123456',
+		'se_reconect' => '4',
+		'reconect' => '1',
+	];
+	/*$configuracion->servidor = 'localhost';
+	$configuracion->nombreBD = 'laboratorio';
+	$configuracion->puerto = '5432';
+	$configuracion->nombreDeUsuario = 'fernando';
+	$configuracion->contrasenia = '123456789';
+	$configuracion->se_reconect = '4';
+	$configuracion->reconect = '1';*/
+?>
